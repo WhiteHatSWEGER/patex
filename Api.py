01b4008b0905710dca9118ff8b020261c304b555
WhiteHatSWEGER <96747701+WhiteHatSWEGER@users.noreply.github.com>
2024-03-10 13:14:40 +0100

    Add files via upload

---
+++ Api.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+class Api:
+    def __init__(self, path_to_data):
+        self.__path_to_data = path_to_data
+    
+    def get_sensors(self):
+        sensors = []
+        if os.path.exists(self.__path_to_data):
+            with os.scandir(self.__path_to_data) as entries:
+                for entry in entries:
+                    sensors.append(entry.name)
+        return sensors
+    
+def add_sensor_data(self, sensor_id, data):
+        if os.path.exists(self.__path_to_data):
+            with open(self.__path_to_data + "/" + str(sensor_id) + ".txt") as file:
+                file.write(json.dumps(data) + "\n")
+                return data
+                
