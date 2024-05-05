f2520eaf702dc10e0b49724f5a3e9cb1945ba63d
WhiteHatSWEGER <96747701+WhiteHatSWEGER@users.noreply.github.com>
2024-05-05 17:11:55 +0200

    Update data_to_csv.py

--- data_to_csv.py
+++ data_to_csv.py
@@ -1,72 +1,79 @@
-import csv
-from datetime import datetime
-# Import sensor modules
-import mq2
-import mq3
-import mq4
-import mq5
-import mq6
-import mq7
-import mq8
-import mq9
-import mq135
+import time
+import math
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
-# Path to the output CSV file
-csv_file_path = 'sensor_readings.csv'
+class MQ135():
+    RL_VALUE = 10  # load resistance in kilo ohms
+    RO_CLEAN_AIR_FACTOR = 3.6  # Sensor resistance in clean air divided by RO
+    CALIBRATION_SAMPLE_TIMES = 50
+    CALIBRATION_SAMPLE_INTERVAL = 50  # in milliseconds
+    READ_SAMPLE_INTERVAL = 50  # in milliseconds
+    READ_SAMPLE_TIMES = 5
 
+    def __init__(self, Ro=10, channel=ADS.P0):
+        self.Ro = Ro
+        i2c = busio.I2C(board.SCL, board.SDA)
+        ads = ADS.ADS1115(i2c, address=0x49)
+        self.chan = AnalogIn(ads, channel)
 
-# Collect data from each sensor module
-def collect_sensor_data():
-    # Instantiate each sensor class
-    mq2 = MQ2()
-    mq3 = MQ3()
-    mq4 = MQ4()
-    mq5 = MQ5()
-    mq6 = MQ6()
-    mq7 = MQ7()
-    mq8 = MQ8()
-    mq9 = MQ9()
-    mq135 = MQ135()
+        print("Calibrating MQ-135 on ADS1115 at address 0x49...")
+        self.Ro = self.MQ135_Calibration()
+        print("Calibration of MQ-135 is done...")
+        print("MQ-135 Ro=%f kohm" % self.Ro)
+        print("\n")
 
-    # Call MQPercentage method on each sensor instance
-    mq2_data = mq2.MQPercentage()
-    mq3_data = mq3.MQPercentage()
-    mq4_data = mq4.MQPercentage()
-    mq5_data = mq5.MQPercentage()
-    mq6_data = mq6.MQPercentage()
-    mq7_data = mq7.MQPercentage()
-    mq8_data = mq8.MQPercentage()
-    mq9_data = mq9.MQPercentage()
-    mq135_data = mq135.MQPercentage()
+    def MQ135_Calibration(self):
+        val = 0.0
+        for i in range(self.CALIBRATION_SAMPLE_TIMES):
+            val += self.MQResistanceCalculation(self.chan.value)
+            time.sleep(self.CALIBRATION_SAMPLE_INTERVAL / 1000.0)
+        val = val / self.CALIBRATION_SAMPLE_TIMES
+        val = val / self.RO_CLEAN_AIR_FACTOR
+        return val
 
-    # Compile data into a single record, ensure order matches CSV format
-    data_record = [
-        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),  # Timestamp
-        mq2_data['LPG'], mq2_data['Propane'], mq2_data['Methane'], mq2_data['Hydrogen'], mq2_data['Smoke'],
-        mq3_data['Alcohol'], mq3_data['Ethanol'],
-        mq4_data['Methane'], mq4_data['NaturalGas'],
-        mq5_data['NaturalGas'], mq5_data['LPG'],
-        mq6_data['LPG'], mq6_data['Butane'], mq6_data['Propane'],
-        mq7_data['CO'],
-        mq8_data['H2'],
-        mq9_data['CO'], mq9_data['FlammableGases'],
-        mq135_data['NH3'], mq135_data['NOx'], mq135_data['C6H6'], mq135_data['Smoke'], mq135_data['CO2']
-    ]
-    return data_record
+    def MQResistanceCalculation(self, raw_adc):
+        if raw_adc == 0:
+            raw_adc = 1
+        return float(self.RL_VALUE * (32767.0 - raw_adc) / float(raw_adc))
 
+    def MQRead(self):
+        rs = 0.0
+        raw_value = 0.0
+        for i in range(self.READ_SAMPLE_TIMES):
+            raw_value += self.chan.value
+            rs += self.MQResistanceCalculation(self.chan.value)
+            time.sleep(self.READ_SAMPLE_INTERVAL / 1000.0)
+        rs = rs / self.READ_SAMPLE_TIMES
+        raw_value = raw_value / self.READ_SAMPLE_TIMES
+        return rs, raw_value
 
-# Write data to the CSV file
-def write_data_to_csv(data_record):
-    with open(csv_file_path, mode='a', newline='') as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(data_record)
+    def MQPercentage(self):
+        val = {}
+        read, raw_value = self.MQRead()
+        val["ACETON"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_ACETON)
+        val["TOLUENO"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_TOLUENO)
+        val["ALCOHOL"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_ALCOHOL)
+        val["CO2"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_CO2)
+        val["NH4"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_NH4)
+        val["CO"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_CO)
+        val["RAW_VALUE"] = raw_value
+        return val
 
+    def MQGetGasPercentage(self, rs_ro_ratio, gas_id):
+        curves = {
+            self.GAS_ACETON: self.ACETONCurve,
+            self.GAS_TOLUENO: self.TOLUENOCurve,
+            self.GAS_ALCOHOL: self.AlcoholCurve,
+            self.GAS_CO2: self.CO2Curve,
+            self.GAS_NH4: self.NH4Curve,
+            self.GAS_CO: self.COCurve
+        }
+        pcurve = curves[gas_id]
+        return (math.pow(10, (((math.log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0])))
 
-def main():
-    data_record = collect_sensor_data()
-    write_data_to_csv(data_record)
-    print(f"Data recorded at {data_record[0]}")
-
-
-if __name__ == '__main__':
-    main()
+# Example of using the MQ135 class
+sensor = MQ135()
+print(sensor.MQPercentage())
