1c8cf7a169178908a171582147339e67d60b4e87
WhiteHatSWEGER <96747701+WhiteHatSWEGER@users.noreply.github.com>
2024-05-05 17:18:20 +0200

    Update mq135.py

--- mq135.py
+++ mq135.py
@@ -28,52 +28,54 @@ class MQ135():
     def MQ135_Calibration(self):
         val = 0.0
         for i in range(self.CALIBRATION_SAMPLE_TIMES):
-            val += self.MQResistanceCalculation(self.chan.value)
+            val += self.MQ_ResistanceCalculation(self.chan.value)
             time.sleep(self.CALIBRATION_SAMPLE_INTERVAL / 1000.0)
         val = val / self.CALIBRATION_SAMPLE_TIMES
         val = val / self.RO_CLEAN_AIR_FACTOR
         return val
 
-    def MQResistanceCalculation(self, raw_adc):
+    def MQ_ResistanceCalculation(self, raw_adc):
         if raw_adc == 0:
             raw_adc = 1
         return float(self.RL_VALUE * (32767.0 - raw_adc) / float(raw_adc))
 
-    def MQRead(self):
+    def MQ_Read(self):
         rs = 0.0
         raw_value = 0.0
         for i in range(self.READ_SAMPLE_TIMES):
             raw_value += self.chan.value
-            rs += self.MQResistanceCalculation(self.chan.value)
+            rs += self.MQ_ResistanceCalculation(self.chan.value)
             time.sleep(self.READ_SAMPLE_INTERVAL / 1000.0)
         rs = rs / self.READ_SAMPLE_TIMES
         raw_value = raw_value / self.READ_SAMPLE_TIMES
         return rs, raw_value
 
-    def MQPercentage(self):
+    def MQ_Percentage(self):
         val = {}
-        read, raw_value = self.MQRead()
-        val["ACETON"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_ACETON)
-        val["TOLUENO"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_TOLUENO)
-        val["ALCOHOL"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_ALCOHOL)
-        val["CO2"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_CO2)
-        val["NH4"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_NH4)
-        val["CO"] = self.MQGetGasPercentage(read / self.Ro, self.GAS_CO)
+        read, raw_value = self.MQ_Read()
+        # Here you would need to add the logic to calculate percentage based on curves for each gas
+        # For example:
+        val["ACETON"] = self.MQ_GetGasPercentage(read / self.Ro, 0)
+        val["TOLUENO"] = self.MQ_GetGasPercentage(read / self.Ro, 1)
+        val["ALCOHOL"] = self.MQ_GetGasPercentage(read / self.Ro, 2)
+        val["CO2"] = self.MQ_GetGasPercentage(read / self.Ro, 3)
+        val["NH4"] = self.MQ_GetGasPercentage(read / self.Ro, 4)
+        val["CO"] = self.MQ_GetGasPercentage(read / self.Ro, 5)
         val["RAW_VALUE"] = raw_value
         return val
 
-    def MQGetGasPercentage(self, rs_ro_ratio, gas_id):
-        curves = {
-            self.GAS_ACETON: self.ACETONCurve,
-            self.GAS_TOLUENO: self.TOLUENOCurve,
-            self.GAS_ALCOHOL: self.AlcoholCurve,
-            self.GAS_CO2: self.CO2Curve,
-            self.GAS_NH4: self.NH4Curve,
-            self.GAS_CO: self.COCurve
-        }
+    def MQ_GetGasPercentage(self, rs_ro_ratio, gas_id):
+        curves = [
+            [1.0,0.18,-0.32],  # ACETON
+            [1.0,0.2,-0.30],   # TOLUENO
+            [1.0,0.28,-0.32],  # ALCOHOL
+            [1.0,0.38,-0.37],  # CO2
+            [1.0,0.42,-0.42],  # NH4
+            [1.0,0.45,-0.26]   # CO
+        ]
         pcurve = curves[gas_id]
-        return (math.pow(10, (((math.log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0])))
+        return math.pow(10, (((math.log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0]))
 
 # Example of using the MQ135 class
 sensor = MQ135()
-print(sensor.MQPercentage())
+print(sensor.MQ_Percentage())
